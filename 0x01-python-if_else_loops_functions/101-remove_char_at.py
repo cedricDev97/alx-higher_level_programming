aa22065501bb3a2948a8cca97ad41ca1999d7e56
cedricDev97 <somcedric002@gmail.com>
2022-06-01 14:31:42 +0000

    statement and loops in python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,6 +4,6 @@ def remove_char_at(str, n):
     if n < 0:
         last_part = str[n-0:]
     else:
-            last_part = str[n+1:]
-        total_part = first_part + last_part
-        return total_part
+        last_part = str[n+1:]
+    total_part = first_part + last_part
+    return total_part
