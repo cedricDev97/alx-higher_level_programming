4cb45f3f4ebd5e30f8ab5cfe3051f38986d534d6
cedricDev97 <99391068+cedricDev97@users.noreply.github.com>
2022-07-06 00:13:42 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,38 @@
 #!/usr/bin/python3
-""" Module to print status code """
+'''
+Script for log parsing
+'''
 import sys
 
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
-
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
-
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
-
-
 if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
-
+    a = []
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    dictio = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
+    i = 0
+    file_size = 0
     try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
-    except KeyboardInterrupt:
-        magic.print_info()
-        raise
-    magic.print_info()
+        for lines in sys.stdin:
+            a = lines.split(" ")
+            if "\n" in a[-1]:
+                value = a[-1][:-1]
+            else:
+                value = a[-1]
+            if len(a) > 2 and a[-2] in codes and value.isnumeric() is True:
+                dictio[a[-2]] += 1
+                file_size += int(value)
+                i += 1
+            if i % 10 == 0:
+                print("File size: {}".format(file_size))
+                for code in codes:
+                    if dictio[code] > 0:
+                        print("{}: {}".format(code, dictio[code]))
+        exit()
+    except Exception:
+        pass
+    finally:
+        print("File size: {}".format(file_size))
+        for code in codes:
+            if dictio[code] > 0:
+                print("{}: {}".format(code, dictio[code]))
