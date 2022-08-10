972953d08b913838f356bb129b1f1b1d3503ffba
cedricDev97 <99391068+cedricDev97@users.noreply.github.com>
2022-08-11 00:16:48 +0100

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,4 @@
-es the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 -- uses a database to list all rows not linked to one row
 SELECT name
 FROM tv_genres
